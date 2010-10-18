071f92e2f4f0bbd8b0b286ad1cc21eac2fa7bb5b
Luke Amdor <lamdor@geolearning.com>
2010-10-18 08:48:04 -0500

    added jvm related variables

---
+++ lib/java.zsh
@@ -0,0 +1,9 @@
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export JAVA_OPTS="-Xms256m -Xmx512m -XX:MaxPermSize=256m -d32 -client"
+
+export SCALA_HOME=/usr/local/Cellar/scala/2.7.5-final/
+export ANDROID_SDK_ROOT=/usr/local/Cellar/android-sdk/r5
+
+export M2_HOME=/usr/local/Cellar/maven/2.2.1/
+export M2_REPO="$HOME/.m2/repository"
+export MAVEN_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m -d32 -client"
