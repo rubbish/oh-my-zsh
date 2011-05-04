773070199a727fffda7d486afdae519793b43c89
Luke Amdor <luke.amdor@t8webware.com>
2011-05-04 09:20:56 -0500

    java.zsh isn't needed anymo

--- lib/java.zsh
+++
@@ -1,7 +0,0 @@
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-export JAVA_OPTS="-Xms256m -Xmx512m -XX:MaxPermSize=256m -d32 -client"
-
-export SCALA_HOME=/usr/local/Cellar/scala/2.7.5-final/
-export ANDROID_SDK_ROOT=/usr/local/Cellar/android-sdk/r5
-
-export MAVEN_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m -d32 -client"
