0635681b176bedbe6989ab49924088e88f5e7c80
Luke Amdor <luke.amdor@t8webware.com>
2011-06-23 09:52:42 -0500

    adding vagrant plugin

--- init.zsh
+++ init.zsh
@@ -21,7 +21,7 @@ export ZSH_THEME="mine"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git command-not-found)
+plugins=(git vagrant command-not-found)
 
 source $ZSH/oh-my-zsh.sh
 
